e8bbed2e2511764589b4481e4df565d98838f894
Sai Emani <semani@yelp.com>
2023-04-07 22:14:22 -0400

    Hilt initialized

--- app/src/main/java/com/saie/dogscompose/DogsApp.kt
+++ app/src/main/java/com/saie/dogscompose/DogsApp.kt
@@ -1,8 +1,10 @@
 package com.saie.dogscompose
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class DogsApp: Application() {
     override fun onCreate() {
         super.onCreate()
