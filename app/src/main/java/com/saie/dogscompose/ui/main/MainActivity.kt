403da62934775c22bbfd6b3b845c2a78e57cffbd
Sai Emani <semani@yelp.com>
2023-04-08 21:37:44 -0400

    Breeds displaying on main page

--- app/src/main/java/com/saie/dogscompose/ui/main/MainActivity.kt
+++ app/src/main/java/com/saie/dogscompose/ui/main/MainActivity.kt
@@ -4,29 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.saie.dogscompose.BuildConfig
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.saie.dogscompose.ui.theme.DogsComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DogsComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            App()
+        }
+    }
+}
+
+@Composable
+fun App(
+    mainViewModel: MainViewModel = hiltViewModel()
+) {
+    DogsComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            val dogBreeds by mainViewModel.breedList.collectAsState(initial = emptyList())
+            LazyColumn {
+                items(dogBreeds) { breed ->
+                    Greeting(name = breed.name)
                 }
             }
         }
@@ -35,8 +50,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-    Timber.d("Prajnan", BuildConfig.DogsApiKey)
+    Text(text = "$name!")
 }
 
 @Preview(showBackground = true)
