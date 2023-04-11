97bfaef71dc4da586081ac2b0cc6b4267a9dba62
Sai Emani <semani@yelp.com>
2023-04-10 21:31:25 -0400

    Navigation to detail page done.

--- app/src/main/java/com/saie/dogscompose/ui/main/MainActivity.kt
+++ app/src/main/java/com/saie/dogscompose/ui/main/MainActivity.kt
@@ -3,16 +3,20 @@ package com.saie.dogscompose.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -22,11 +26,17 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import coil.compose.AsyncImage
 import com.saie.dogscompose.data.Breed
 import com.saie.dogscompose.ui.theme.DogsComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import androidx.compose.foundation.rememberScrollState
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -42,6 +52,45 @@ class MainActivity : ComponentActivity() {
 fun App(
     mainViewModel: MainViewModel = hiltViewModel()
 ) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavScreen.Home.route) {
+        composable(NavScreen.Home.route) {
+            Breeds(
+                mainViewModel,
+                selectBreed = {
+                    navController.navigate("${NavScreen.BreedDetails.route}/$it")
+                }
+            )
+
+            LaunchedEffect(Unit) {
+                Timber.d("Navigating to home")
+            }
+        }
+
+        composable(
+            route = NavScreen.BreedDetails.routeWithArgument,
+            arguments = listOf(navArgument(NavScreen.BreedDetails.argument0) { type = NavType.IntType}
+            )
+        ) { backStackEntry ->
+            val breedId = backStackEntry.arguments?.getInt(NavScreen.BreedDetails.argument0)
+                ?: return@composable
+
+            BreedDetails(
+                breedId = breedId,
+                viewModel = mainViewModel
+            ) {
+                navController.navigateUp()
+            }
+        }
+    }
+}
+
+@Composable
+fun Breeds(
+    mainViewModel: MainViewModel,
+    selectBreed: (Int) -> Unit
+) {
     DogsComposeTheme {
         // A surface container using the 'background' color from the theme
         Surface(
@@ -51,7 +100,7 @@ fun App(
             val dogBreeds by mainViewModel.breedList.collectAsState(initial = emptyList())
             LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 200.dp)) {
                 items(dogBreeds) { breed ->
-                    Breed(breed)
+                    Breed(breed, selectBreed)
                 }
             }
         }
@@ -59,13 +108,14 @@ fun App(
 }
 
 @Composable
-fun Breed(breed: Breed) {
+fun Breed(breed: Breed, selectBreed: (Int) -> Unit) {
     Card (
         backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
             .width(200.dp)
             .height(250.dp)
+            .clickable(onClick = { selectBreed(breed.id) })
     ) {
         CardContent(breed = breed)
     }
@@ -95,10 +145,29 @@ fun CardContent(breed: Breed) {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DogsComposeTheme {
-        //Greeting("Android")
+fun BreedDetails(breedId: Int,
+                 viewModel: MainViewModel,
+                 pressOnBack: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .background(MaterialTheme.colors.background)
+            .fillMaxHeight()
+    ) {
+        Text(text = breedId.toString())
+    }
+}
+
+sealed class NavScreen(val route: String) {
+
+    object Home : NavScreen("Home")
+
+    object BreedDetails : NavScreen("BreedDetails") {
+
+        const val routeWithArgument: String = "BreedDetails/{breedId}"
+
+        const val argument0: String = "breedId"
     }
 }
